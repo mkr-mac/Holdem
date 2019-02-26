fcb0776faf161dfbcafb81cc0fa67c19adb30167
Mickey McCargish <mccargishm@gmail.com>
2019-02-26 08:28:13 -0500

    Removed unused file

--- Holdem/AdjucatorTest.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include "card.h"
-
-TEST(junkHighCard, Twos) {
-	std::vector<Card> two { Card(2,2),Card(2,2),Card(2,2),Card(2,2),Card(2,2) };
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
-}
